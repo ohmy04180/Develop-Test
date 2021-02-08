cd1f1c23cd066d1d2d7275512b0012de13a7fa68
ohmy04180 <ohmy0418@mobigen.com>
2021-02-08 15:20:06 +0900

    Resolve: api modify

--- api.js
+++ api.js
@@ -1,41 +1,34 @@
 const refreshServerInfo = () => {
-  const req = new XMLHttpRequest()
-
-  console.log(req);
-  console.log(req.status);
-
-  req.open('GET', 'https://www.icheon.go.kr/portal/rssBbsNtt.do?bbsNo=13&type=p', true)
-  req.setRequestHeader('Content-type', 'application/json');
-  // req.withCredentials = true;
-  req.send();
+  const xhr = new XMLHttpRequest();
 
   // load 이벤트는 서버 응답이 완료된 경우에 발생한다.
-  req.onreadystatechange = function () {
+  xhr.onreadystatechange = () => {
+
     try {
       // 서버 응답 완료 && 정상 응답
-      if (req.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
 
       // status는 response 상태 코드를 반환 : 200 => 정상 응답
-      if (req.status === 200) {
+      if (xhr.readyState === 4 && xhr.status === 200) {
 
-        // const serverInfo = document.querySelector(".test");
+        console.log(`xhr.status is: ${xhr.status}`);
 
-        // Object.keys(data).forEach(p => {
-        //   const replacements = serverInfo.querySelectorAll(`[data-replace = "${p}]`);
+        const parseData = JSON.parse(xhr.responseText);
+        console.log(typeof parseData, parseData);
 
-        //   for(let r of replacements) {
-        //     r.textContent = data[p];
-        //   }
-        // })
-        console.log(req.responseText);
+        // document.getElementById("demo").innerHTML = parseData.totalCount;
+    
       } else {
-        console.log(`[${req.status}] : ${req.statusText}`);
+        console.log(`[${xhr.status}] : ${xhr.statusText}`);
       }
     } catch (e) {
       return Error('Caught Exception: ' + e.description);
 
     }
   };
+
+  xhr.open('GET', 'https://www.icheon.go.kr/portal/rssBbsNtt.do?bbsNo=13&type=p', true)
+  xhr.send();
 }
 
 const init = () => {
