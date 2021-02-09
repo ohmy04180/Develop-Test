4606e75de708da2d36e982aab50f37adef3e2dfc
ohmy04180 <ohmy0418@mobigen.com>
2021-02-09 18:38:31 +0900

    Resolve: 스타일, 마크업 수정, api 데이터 가져오기 성공

--- api.js
+++ api.js
@@ -1,38 +1,69 @@
-const refreshServerInfo = () => {
+let testData;
+
+
+const showTestData = () => {
+  console.log(`--- Second Function Start`);
+  console.log(typeof testData, testData);
+
+  for(let e = 1; e <= 10; e++){
+    console.log(e)
+
+  }
+
+  for (let i = 0; i <= testData.length; i++) {
+    const tbody = document.querySelector("#tableBody");
+    const tableList = `
+    <tr id=tableList${i+1}>
+      <td id="dataUserId">
+        <span class="table__text">${testData[i].userId}</span>
+      </td>
+      <td id="dataId">
+        <span class="table__text">${testData[i].id}</span>
+      </td>
+      <td id="dataTitle">
+        <span class="table__text">${testData[i].title}</span>
+      </td>
+      <td id="dataText">
+        <span class="table__text">${testData[i].body}</span>
+      </td>
+    </tr>`;
+
+    tbody.innerHTML += tableList;
+  }
+};
+
+const bringJsonData = () => {
   const xhr = new XMLHttpRequest();
 
   // load 이벤트는 서버 응답이 완료된 경우에 발생한다.
   xhr.onreadystatechange = () => {
-
     try {
       // 서버 응답 완료 && 정상 응답
       if (xhr.readyState !== XMLHttpRequest.DONE) return;
 
       // status는 response 상태 코드를 반환 : 200 => 정상 응답
       if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log(`xhr.status is ${xhr.status}`);
 
-        console.log(`xhr.status is: ${xhr.status}`);
+        // 데이터를 새로 담아준다.
+        testData = JSON.parse(xhr.responseText);
 
-        const parseData = JSON.parse(xhr.responseText);
-        console.log(typeof parseData, parseData);
-
-        // document.getElementById("demo").innerHTML = parseData.totalCount;
-    
+        // testData를 HTML Element로 담아주는 함수 실행
+        showTestData();
       } else {
         console.log(`[${xhr.status}] : ${xhr.statusText}`);
       }
     } catch (e) {
-      return Error('Caught Exception: ' + e.description);
-
+      return Error("Caught Exception: " + e.description);
     }
   };
 
-  xhr.open('GET', 'https://www.icheon.go.kr/portal/rssBbsNtt.do?bbsNo=13&type=p', true)
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
   xhr.send();
-}
+};
 
 const init = () => {
-  refreshServerInfo()
-}
+  bringJsonData();
+};
 
-init()
+init();
