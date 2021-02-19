1dd1951f313f140f21e7f0a14dd64ae60857b7df
ohmy04180 <ohmy0418@mobigen.com>
2021-02-19 17:18:37 +0900

    Resolve: api 코드 수정

--- src/js/api.js
+++ src/js/api.js
@@ -8,57 +8,66 @@ let endNumber;
 
 const showDataList = () => {
   console.log(`--- Third Function Start`)
+  console.log(`pageNumber!!!! is ${pageNumber}`)
 
   prevButton.addEventListener('click', (e) => {
     e.preventDefault();
     pageNumber--;
-    console.log(pageNumber)
 
     startNumber = 10 * (pageNumber);
     endNumber = (10 * (pageNumber)) + 9;
 
-    if (pageNumber < 10 && pageNumber > 0) {
+    if (pageNumber === -1) {
+      pageNumber = 0;
+      e.target.disabled = true;
+      console.log("error")
+    } else {
+      console.log(`pageNumber is ${pageNumber}`)
       console.log(`startNumber is ${startNumber}`)
       console.log(`endNumber is ${endNumber}`)
 
+      nextButton.disabled = false;
       tbody.innerHTML = '';
       for (let num = startNumber; num <= endNumber; num++) {
         const tableList = `
-            <tr id=tableList${num + 1}>
-              <td id="dataUserId">
-                <span class="table__text">${testData[num].userId}</span>
-              </td>
-              <td id="dataId">
-                <span class="table__text">${testData[num].id}</span>
-              </td>
-              <td id="dataTitle">
-                <span class="table__text">${testData[num].title}</span>
-              </td>
-              <td id="dataText">
-                <span class="table__text">${testData[num].body}</span>
-              </td>
-            </tr>`
+                <tr id=tableList${num + 1}>
+                  <td id="dataUserId">
+                    <span class="table__text">${testData[num].userId}</span>
+                  </td>
+                  <td id="dataId">
+                    <span class="table__text">${testData[num].id}</span>
+                  </td>
+                  <td id="dataTitle">
+                    <span class="table__text">${testData[num].title}</span>
+                  </td>
+                  <td id="dataText">
+                    <span class="table__text">${testData[num].body}</span>
+                  </td>
+                </tr>`
         tbody.innerHTML += tableList
       }
-    } else {
-      console.log("error")
-      e.target.disabled = true;
-      nextButton.disabled = false
     }
   })
 
   nextButton.addEventListener('click', (e) => {
     e.preventDefault;
     pageNumber++;
-    console.log(pageNumber)
-
     startNumber = 10 * (pageNumber);
     endNumber = (10 * (pageNumber)) + 9;
 
-    if (pageNumber < 10) {
+
+    if (pageNumber === 10) {
+      pageNumber = 9;
+      e.target.disabled = true;
+      console.log("error")
+    } else {
+      console.log(`pageNumber is ${pageNumber}`)
       console.log(`startNumber is ${startNumber}`)
       console.log(`endNumber is ${endNumber}`)
+      
+      prevButton.disabled = false;
       tbody.innerHTML = '';
+
       for (let num = startNumber; num <= endNumber; num++) {
         const tableList = `
             <tr id=tableList${num + 1}>
@@ -77,13 +86,8 @@ const showDataList = () => {
             </tr>`
         tbody.innerHTML += tableList
       }
-    } else {
-      console.log("error")
-      e.target.disabled = true;
     }
   })
-
-
 }
 
 const showDefaultDataList = () => {
